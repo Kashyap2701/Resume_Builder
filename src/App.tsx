f261617e772028dec67286f2c9c897446f583e30
Kashyap Patel <kashyap.p@simformsolutions.com>
2023-06-07 11:47:13 +0530

    Create leading page on home UI

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import { Route, Routes } from "react-router";
 import Login from "./Pages/Auth/Login";
 import { Signup } from "./Pages/Auth/Signup";
 import CreateResume from "./Pages/Create Resume/CreateResume";
+import Home from "./Pages/Home/Home";
 
 function App() {
   return (
     <div className="container">
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/create-resume" element={<CreateResume />} />
