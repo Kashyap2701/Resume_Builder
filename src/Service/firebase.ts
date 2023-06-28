508405b7145614fe379935eae32e6e9022748d5c
Kashyap Patel <kashyap.p@simformsolutions.com>
2023-06-28 11:25:03 +0530

    Add comment to firebase.ts file

--- src/Service/firebase.ts
+++ src/Service/firebase.ts
@@ -4,6 +4,7 @@ import { getDownloadURL, getStorage, ref, uploadBytes } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 import uuid from "react-uuid";
 
+// All the configuration for firebase
 const firebaseConfig = {
   apiKey: "AIzaSyAYkxnsMKdXVWq-CrQnY45_pFRq9iFmnLo",
   authDomain: "resume-builder-a08b1.firebaseapp.com",
@@ -13,16 +14,25 @@ const firebaseConfig = {
   appId: "1:930601004657:web:38c2c548e58b140b22b5d0",
 };
 
+// Initialize the Firebase app using the provided configuration
 const app = initializeApp(firebaseConfig);
+
+// Get the authentication service instance from the initialized app
+const auth = getAuth(app);
+
+// Get the Firestore database instance from the initialized app
+const db = getFirestore(app);
+
+// Get the Firebase Storage instance
 const storage = getStorage();
 
-export const uploadPhoto = async (uploadImg:Blob) => {
+// Utility function that upload photo in firebase ang get URL
+export const uploadPhoto = async (uploadImg: Blob) => {
   const imageRef = ref(storage, `profilePhoto/${uuid() + uploadImg.name}`);
   await uploadBytes(imageRef, uploadImg);
   const downloadURL = await getDownloadURL(imageRef);
   return downloadURL;
 };
 
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+export { auth, db };
 export default app;
